8df6152dc115f64ffce417015630de7392534fc3
David Hoy <david@thehoys.com>
2020-06-28 15:52:44 -0400

    Added notes to README.md Cleaned up the code, added comments, removed my MT IP address and port #.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rpi-ais
 AIS Forwarder for MarineTraffic
+
+This is a very simple AIS forwarder for MarineTraffic.com.  It connects to an AIS transponder that sends NMEA 0183 formatted AIS messages over a TCP port, filters the data stream to remove any non-AIS messages, and forwards the resulting stream to a UDP port on a MarineTraffic server.  
+
+The script will run on any machine that has a Python 3 interpreter, though I run it on a dedicated Raspberry Pi Zero W.  You will need to get your own IP address and port number from MarineTraffic, and modify the appropriate line in the script to use the info they provide for you.  DO NOT use the script unmodified.
+
+I have only tested this with a Vesper XB-8000 AIS transponder, though it should work with any other device that sends NMEA 0183 message over a TCP connection.  It should be pretty simple to modify to work over a UART or USB serial connection.
+
+If you have any problems or questions, please email me at david@thehoys.com.
+
+Regards,
+David Hoy
+S/V Rising Sun
+Catalina 470, hull #17
--- rpi-ais.py
+++ rpi-ais.py
@@ -1,23 +1,36 @@
+# rpi-ais.py: Simple AIS forwarder for MarineTraffic.com
+#
+# Should run on an machine with a Python 3 interpreter, but
+# tested extensively on a Raspberry Pi Zero W.
+#
+# This assumes that you have alreadty configured you Pi
+# to connect to your local WiFi network.
+
 import socket
 import sys
 
-# Create a TCP/IP socket
+# Create a TCP socket, and connect to AIS data stream
 ais_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Connect the socket to the port where the server is listening
-ais_address = ('192.168.1.24', 39150)
+ais_address = ('<insert Vesper IP address here>', 39150)
 print('Connecting to AIS at %s port %s' % ais_address)
 ais_sock.connect(ais_address)
 ais_file = ais_sock.makefile()
 
+# Create socket for UDP connection to MarineTraffic.com server
+# MT will assign you an IP address and port number when you
+# register your station with them.
 mt_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-mt_address = ('5.9.207.224', 10170)
+mt_address = ('<insert MT IP address here>', <insert MT port # here>)
 print('Connecting to MarineTraffic at %s port %s' % mt_address)
 
+# Loop forever
 try:
     while True:
+        # Read a NMEA 0183 formatted string from the AIS transponder
         str = ais_file.readline()
+        # Filter out any messages that do not start with !AIVDM or !AIVDO
         if str.startswith( '!AIVDM') or str.startswith( '!AIVDO'):
+            # Echo to console, and send the string to the UDP port
             print(str, end="")
             data = bytes(str, 'utf-8')
             mt_sock.sendto(data, mt_address)
@@ -25,3 +38,4 @@ try:
 finally:
     print('Closing sockets')
     ais_sock.close()
+    mt_sock.close()
